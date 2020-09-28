abe497e8ec6b779e4c812e79b83e26687afcfd40
johanssonida1996@gmail.com <johanssonida1996@gmail.com>
2020-09-28 09:52:13 +0200

    Add project files.

---
+++ SharedLibaries/Services/DeviceServices.cs
@@ -0,0 +1,60 @@
+﻿using SharedLibaries.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MAD = Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using SharedLibaries.Services;
+
+
+namespace SharedLibaries.Services
+{
+    public static class DeviceServices
+    {
+        private static readonly Random rnd = new Random();
+        public static async Task SendMessageAsync(DeviceClient deviceClient)
+        {
+            while (true)
+            {
+                var data = new TempratureModel
+                {
+                    Temperature = rnd.Next(20, 30),
+                    Humidity = rnd.Next(40, 50)
+                };
+
+                var json = JsonConvert.SerializeObject(data);
+
+                var payload = new Message(Encoding.UTF8.GetBytes(json));
+                await deviceClient.SendEventAsync(payload);
+
+                Console.WriteLine($"Message sent: {json}");
+
+                await Task.Delay(60 * 1000);
+            }
+        }
+
+        public static async Task ReceiveMessageAsync(DeviceClient deviceClient)
+        {
+            while (true)
+            {
+                var payload = await deviceClient.ReceiveAsync();
+
+                if (payload == null)
+                    continue;
+
+                Console.WriteLine($"Message Received {Encoding.UTF8.GetString(payload.GetBytes())}");
+
+                await deviceClient.CompleteAsync(payload);
+            }
+        }
+
+        public static async Task SendMessageToDeviceAsync(MAD.ServiceClient serviceClient, string targetDeviceId, string message)
+        {
+            var payload = new MAD.Message(Encoding.UTF8.GetBytes(message));
+            await serviceClient.SendAsync(targetDeviceId, payload);
+        }
+    }
+}
---
+++ UWPIOT_4/AzureHelpers/AzureProgram.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Azure.Devices.Client;
+using SharedLibaries.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPIOT_4.AzureHelpers
+{
+    public class AzureProgram
+    {
+        private static readonly string _conn = "HostName=ec-win20-iothubida.azure-devices.net;DeviceId=consoleapp;SharedAccessKey=ejgLOFauDEcazZlOsZ4DJQ/QJsSIZ0O1j6EmKwcpKdI=";
+
+        private static readonly DeviceClient deviceClient = DeviceClient.CreateFromConnectionString(_conn, TransportType.Mqtt);
+
+        public static void SendReceiveMessageAsync()
+        {
+
+            DeviceServices.SendMessageAsync(deviceClient).GetAwaiter();
+            DeviceServices.ReceiveMessageAsync(deviceClient).GetAwaiter();
+
+        }
+    }
+}
